13b4c719aebf0f6d5b3acb3ca5b5288051232105
2wiktor2 <wiktor2011@gmail.com>
2019-12-09 23:45:13 +0300

    lesson 37 finished

--- app/src/main/java/com/wiktor/udemyrazdel5/MainActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel5/MainActivity.java
@@ -2,25 +2,31 @@ package com.wiktor.udemyrazdel5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText editTextMessage;
     Button buttonSend;
-    TextView textViewMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextMessage = findViewById(R.id.et_massage);
+        buttonSend = findViewById(R.id.b_send_message);
+        buttonSend.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        String msg = editTextMessage.getText().toString();
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        intent.putExtra("msg", msg);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/wiktor/udemyrazdel5/ReceivedMessageActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel5/ReceivedMessageActivity.java
@@ -2,13 +2,23 @@ package com.wiktor.udemyrazdel5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceivedMessageActivity extends AppCompatActivity {
+    TextView textViewMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_message);
+
+        textViewMessage = findViewById(R.id.tv_received_message);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
+
+        textViewMessage.setText(msg);
     }
 }
